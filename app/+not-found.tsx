909e22c8fcf6dc5e29db698b5ea136c7d5e3a5c5
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-09-17 15:18:07 +0600

    updated all the basic files setting

---
+++ app/+not-found.tsx
@@ -0,0 +1,13 @@
+
+import { View, Text } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <View>
+        <Text>Not found</Text>
+      </View>
+    </>
+  );
+}
+
