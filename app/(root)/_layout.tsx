8ce9d3672e86e040e851441608681678c54f112d
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-09-18 14:34:17 +0600

    customButton

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const rootLayout = () => {
+    return (
+        <View>
+            <Text>RootLayout</Text>
+        </View>
+    )
+}
+
+export default rootLayout 
