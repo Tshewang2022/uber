301086cc83b82c0a0c538f5d1b53e416123d82f1
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-09-21 14:58:36 +0600

    now is the time to intergrate the google service api by going through the google cloud console

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,6 +1,10 @@
+import GoogleTextInput from '@/components/GoogleTextInput'
+import Maps from '@/components/Maps'
 import RideCard from '@/components/RideCard'
+import { icons, images } from '@/constants'
 import { SignedIn, useUser } from '@clerk/clerk-expo'
-import { FlatList } from 'react-native'
+import { isLoaded } from 'expo-font'
+import { ActivityIndicator, ActivityIndicatorBase, FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const mockRides = [
@@ -103,10 +107,60 @@ const mockRides = [
 ]
 export default function Page() {
     const { user } = useUser()
+    const loading = true;
+    const handleSignOut = () => {
+
+    }
+    const handleDestinationPress = () => {
+
+    }
 
     return (
         <SafeAreaView className='bg-general-500'>
-            <FlatList data={mockRides} renderItem={({ item }) => <RideCard ride={item} />} />
+            <FlatList
+                data={mockRides?.slice(0, 5)}
+                // data={[]}
+                renderItem={({ item }) => <RideCard ride={item} />}
+                className='px-5'
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ paddingBottom: 100 }}
+                ListEmptyComponent={() => (
+                    <View className='flex flex-col items-center justify-center'>
+                        {!loading ? (<>
+                            <Image source={images.noResult} className='w-40 h-40' resizeMode='contain' />
+                            <Text className='text-sm'>No recent rides found</Text>
+                        </>) : (<ActivityIndicator size='small' color='#000' />)}
+                    </View>
+                )}
+                ListHeaderComponent={() => (
+                    <>
+                        <View className="flex flex-row items-center justify-between my-5">
+                            <Text className='text-xl capitalize font-JakartaExtraBold'>Welcome {user?.firstName} 👋</Text>
+                            <TouchableOpacity onPress={handleSignOut} className='justify-center items-center w-10 h-10 rounded-full bg-white'>
+                                <Image source={icons.out} className='w-4 h-4' />
+                            </TouchableOpacity>
+                        </View>
+                        {/* googleTextInput */}
+                        <GoogleTextInput
+                            icon={icons.search}
+                            containerStyle="bg-white shadow-md shadow-neutral-300"
+                            handlePress={handleDestinationPress}
+                        />
+                        <>
+                            <Text className='text-xl font-JakartaBold mt-5 mb-3'>
+                                Your current location
+                            </Text>
+                            <View className="flex flex-row items-center bg-transparent h-[300px]">
+                                {/* map section */}
+                                <Maps />
+                            </View>
+                        </>
+                        <Text className='text-xl font-JakartaBold mt-5 mb-3'>
+                            Recent Rides
+                        </Text>
+                    </>
+                )} />
         </SafeAreaView>
     )
+
 }
--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,6 +1,8 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Ride } from '@/types/type'
+import { icons } from '@/constants'
+import { formatDate, formatTime } from '@/lib/utils'
 
 const RideCard = ({ ride: {
     destination_latitude, destination_longitude, origin_address,
@@ -9,14 +11,43 @@ const RideCard = ({ ride: {
 } }: { ride: Ride }) => {
     return (
         <View className='flex flex-row items-center justify-center bg-white rounded-lg shadown-sm shadow-neutral-300 mb-3'>
-            <View className="flex flex-row items-center justify-between p-3">
+            <View className="flex flex-col items-center justify-center p-3">
                 <View className="flex flex-row items-center justify-betweeb">
                     <Image source={{
-                        uri: `https://maps.geopify`
-                    }} />
+                        uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:
+                        ${destination_longitude}, ${destination_latitude}&zoom=12&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}
+                        `
+                    }} className='w-[80px] h-[90px] rounded-lg' />
+                    <View className="flex flex-col mx-5 gap-y-5 flex-1">
+                        <View className="flex flex-row items-center gap-x-2">
+                            <Image source={icons.to} className='w-5 h-5' />
+                            <Text className='text-md' numberOfLines={1}>{origin_address}</Text>
+                        </View>
+                        <View className="flex flex-row items-center gap-x-2">
+                            <Image source={icons.point} className='w-5 h-5' />
+                            <Text className='text-md' numberOfLines={1}>{destination_address}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View className="flex flex-col w-full mt-5 bg-general-500 rounded-lg p-3 items-start justify-center">
+                    <View className="flex flex-row items-center w-full justify-between mb-5">
+                        <Text className='text-md font-JakartaMedium text-gray-500'>Data & Time</Text>
+                        <Text className='text-md font-JakartaMedium text-gray-500'>{formatDate(created_at)}, {formatTime(ride_time)}</Text>
+                    </View>
+                    <View className="flex flex-row items-center w-full justify-between mb-5">
+                        <Text className='text-md font-JakartaMedium text-gray-500'>Driver</Text>
+                        <Text className='text-md font-JakartaMedium text-gray-500'>{driver.first_name} {driver.last_name}</Text>
+                    </View>
+                    <View className="flex flex-row items-center w-full justify-between mb-5">
+                        <Text className='text-md font-JakartaMedium text-gray-500'>Car Seats</Text>
+                        <Text className='text-md font-JakartaMedium text-gray-500'>{driver.car_seats}</Text>
+                    </View>
+                    <View className="flex flex-row items-center w-full justify-between mb-5">
+                        <Text className='text-md font-JakartaMedium text-gray-500'>Payment Status</Text>
+                        <Text className={`text-md capitalize font-JakartaMedium text-gray-500 ${payment_status === 'paid' ? 'text-green-500' : 'text-red-500'}`}>{payment_status}</Text>
+                    </View>
                 </View>
             </View>
-            <Text className='text-3xl'>{driver.first_name}</Text>
         </View>
     )
 }
